4c74f0ed419dbad5946b501d273bcadf4485b7fa
paransaik <jty419@naver.com>
2026-01-22 20:30:10 +0900

    feat: add template auth service

---
+++ api/auth/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("zqksk-api-gateway.java-conventions")
+}
+
+dependencies {
+    implementation(project(":domain:user"))
+    implementation(project(":storage:database"))
+    implementation(project(":support:core-exception"))
+    implementation(project(":support:core-web"))
+    implementation(project(":support:logging"))
+    implementation(project(":support:mail"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("com.github.ben-manes.caffeine:caffeine")
+    implementation("com.fasterxml.uuid:java-uuid-generator:5.0.0")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.getByName("bootJar") {
+    enabled = true
+}
+
+tasks.getByName("jar") {
+    enabled = false
+}
